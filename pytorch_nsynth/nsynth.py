9e8519f8c4b3d5b4f75206f98184562028022365
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2018-11-13 11:37:36 -0500

    add blacklist mechanism and classification target mechanism

--- pytorch_nsynth/nsynth.py
+++ pytorch_nsynth/nsynth.py
@@ -10,8 +10,12 @@ and target_transform callbacks as ususal.
 import os
 import json
 import glob
+import numpy as np
 import scipy.io.wavfile
+import torch
 import torch.utils.data as data
+import torchvision.transforms as transforms
+from sklearn.preprocessing import LabelEncoder
 
 
 class NSynth(data.Dataset):
@@ -24,16 +28,46 @@ class NSynth(data.Dataset):
                 a sample and returns a transformed version.
         target_transform (callable, optional): A function/transform that takes
             in the target and transforms it.
+        blacklist_pattern: list of string used to blacklist dataset element.
+            If one of the string is present in the audio filename, this sample
+            together with its metadata is removed from the dataset.
+        categorical_field_list: list of string. Each string is a key like
+            instrument_family that will be used as a classification target.
+            Each field value will be encoding as an integer using sklearn
+            LabelEncoder.
     """
 
-    def __init__(self, root, transform=None, target_transform=None):
+    def __init__(self, root, transform=None, target_transform=None,
+                 blacklist_pattern=[],
+                 categorical_field_list=["instrument_family"]):
         """Constructor"""
+        assert(isinstance(root, str))
+        assert(isinstance(blacklist_pattern, list))
+        assert(isinstance(categorical_field_list, list))
         self.root = root
         self.filenames = glob.glob(os.path.join(root, "audio/*.wav"))
         with open(os.path.join(root, "examples.json"), "r") as f:
             self.json_data = json.load(f)
+        for pattern in blacklist_pattern:
+            self.filenames, self.json_data = self.blacklist(
+                self.filenames, self.json_data, pattern)
+        self.categorical_field_list = categorical_field_list
+        self.le = []
+        for i, field in enumerate(self.categorical_field_list):
+            self.le.append(LabelEncoder())
+            field_values = [value[field] for value in self.json_data.values()]
+            self.le[i].fit(field_values)
         self.transform = transform
         self.target_transform = target_transform
+
+    def blacklist(self, filenames, json_data, pattern):
+        filenames = [filename for filename in filenames
+                     if pattern not in filename]
+        json_data = {
+            key: value for key, value in json_data.items()
+            if pattern not in key
+        }
+        return filenames, json_data
 
     def __len__(self):
         return len(self.filenames)
@@ -43,18 +77,34 @@ class NSynth(data.Dataset):
         Args:
             index (int): Index
         Returns:
-            tuple: (wav_data, json_data)
+            tuple: (audio sample, *categorical targets, json_data)
         """
         name = self.filenames[index]
         _, sample = scipy.io.wavfile.read(name)
         target = self.json_data[os.path.splitext(os.path.basename(name))[0]]
+        categorical_target = [
+            le.transform([target[field]])[0]
+            for field, le in zip(self.categorical_field_list, self.le)]
         if self.transform is not None:
             sample = self.transform(sample)
         if self.target_transform is not None:
             target = self.target_transform(target)
-        return sample, target
+        return [sample, *categorical_target, target]
 
 
 if __name__ == "__main__":
-    dataset = NSynth("data/nsynth-test")
-    print(dataset[0])
+    # audio samples are loaded as an int16 numpy array
+    # rescale intensity range as float [-1, 1]
+    toFloat = transforms.Lambda(lambda x: x / np.iinfo(np.int16).max)
+    # use instrument_family and instrument_source as classification targets
+    dataset = NSynth(
+        "data/nsynth-test",
+        transform=toFloat,
+        blacklist_pattern=["string"],  # blacklist string instrument
+        categorical_field_list=["instrument_family", "instrument_source"])
+    loader = data.DataLoader(dataset, batch_size=32, shuffle=True)
+    for samples, instrument_family_target, instrument_source_target, targets \
+            in loader:
+        print(samples.shape, instrument_family_target.shape,
+              instrument_source_target.shape)
+        print(torch.min(samples), torch.max(samples))
